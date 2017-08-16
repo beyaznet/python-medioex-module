4aeb9a8667d52e4e2080ddf27d14c346ebd19eab
emrah <emrah.com@gmail.com>
2017-08-16 16:35:20 +0300

    analog input order, revert

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ module = Extension('medioex',
                    )
 
 setup(name='medioex',
-      version='0.1.10',
+      version='0.1.11',
       description='Python MedIOEx module',
       url = 'https://github.com/beyaznet/python-medioex-module',
       author = 'Beyaz R&D Team',
